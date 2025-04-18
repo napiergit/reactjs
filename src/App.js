a217c07fd1f13bdba9260b755910d84c87f26c8e
Andrew <napierandr@gmail.com>
2025-04-17 21:58:01 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Home from "./Home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/reactjs">
       <Routes>
         <Route path="/" element={<div>1</div>} />
-        <Route path="/reactjs" element={<div>2</div>} />
-        <Route path="/reactjs/" element={<Home />} />
+        <Route path="/2/" element={<div>2</div>} />
+        <Route path="/3" element={<div>3</div>} />
+        <Route path="/home/" element={<Home />} />
         <Route path="/reactjs/legacy" element={<OldApp />} />
+        <Route path="/reactjs/1" element={<div>1</div>} />
       </Routes>
     </BrowserRouter>
   );
