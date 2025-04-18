05b732dec669e1f925b4c388e4e75a5aba6928ca
Andrew <napierandr@gmail.com>
2025-04-17 22:08:58 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, BrowserRouter, Routes, Route } from 'react-router-dom';
 import OldApp from "./OldApp";
 import Home from "./Home";
 
 function App() {
   return (
-    <BrowserRouter basename="/reactjs">
+    <HashRouter basename="/reactjs">
       <Routes>
         <Route path="/" element={<div>1</div>} />
         <Route path="/2/" element={<div>2</div>} />
@@ -14,7 +14,7 @@ function App() {
         <Route path="/reactjs/legacy" element={<OldApp />} />
         <Route path="/reactjs/1" element={<div>1</div>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
